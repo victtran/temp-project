1cfc568556ca407e2e63bbc0035637f7c9850c44
victtran <85677014+victtran@users.noreply.github.com>
2023-12-14 16:24:51 -0600

    Update csv_keyword.py

--- csv_keyword.py
+++ csv_keyword.py
@@ -1,16 +1,26 @@
 import pandas as pd
+from fuzzywuzzy import process
 
-def search_keyword_in_dataframe(csv_path, keyword):
+def fuzzy_search_keyword_in_dataframe(csv_path, keyword):
     # Read CSV file into a DataFrame
     df = pd.read_csv(csv_path)
 
-    # Iterate over rows and print rows containing the keyword
-    for index, row in df.iterrows():
-        if keyword.lower() in row.astype(str).str.lower().values:
-            print(f"Row {index + 1}:", row)
+    # Perform fuzzy matching to find the best match
+    best_match, score = process.extractOne(keyword, df.values.flatten(), scorer=fuzzywuzzy.fuzz.token_sort_ratio)
+
+    # Print the best match and its score
+    print(f"Best Match: {best_match}")
+    print(f"Score: {score}")
+
+    # Search for rows containing the best match
+    matching_rows = df[df.apply(lambda row: best_match in row.astype(str), axis=1)]
+
+    # Print the matching rows
+    print("\nMatching Rows:")
+    print(matching_rows)
 
 # Example usage
 csv_file_path = "your_file.csv"  # Replace with the path to your CSV file
 search_keyword = "your_keyword"  # Replace with the keyword you're searching for
 
-search_keyword_in_dataframe(csv_file_path, search_keyword)
+fuzzy_search_keyword_in_dataframe(csv_file_path, search_keyword)
